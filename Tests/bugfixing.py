ecf701420721230c2a469fea1bd22b913411bc35
Otte Heinävaara <gtrrebel@gmail.com>
2015-06-15 17:14:55 +0300

    bashtesting

--- Tests/bugfixing.py
+++ Tests/bugfixing.py
@@ -1,6 +1,6 @@
-def test():
-	for i in range(2):
-		print 'first ', i
-        print 'second ', i
+from ukko_runner import ukko_runner
 
-test()
+runner = ukko_runner.runner()
+runner.MAXLOAD = 1
+print runner.check_slots()
+runner.add_jobs([('github/pol2rna/matlab/run_ode_savesamples.sh %(index)s:%(count)s:100000', 1)])
