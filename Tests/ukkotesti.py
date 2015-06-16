034c191d4249dd89b995cf9a503c86ed5c4fa724
Otte E Heinavaara <othe@ukko177.hpc.cs.helsinki.fi>
2015-06-16 16:13:55 +0300

    ukkotesti.py

---
+++ Tests/ukkotesti.py
@@ -0,0 +1,12 @@
+import numpy
+import scipy
+import sympy
+import theano
+import GPy
+import sys
+sys.path.append('/home/fs/othe/Windows/Desktop/hiit/HIITS15/Moduleita/pyautodiff-python2-ast')
+import autodiff
+
+import pkg_resources
+print pkg_resources.get_distribution("GPy").version
+
