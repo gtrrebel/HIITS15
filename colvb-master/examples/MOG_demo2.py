471881989aec4a2198d7516a856b5e533399b5dc
Otte Heinävaara <gtrrebel@gmail.com>
2015-06-16 17:23:18 +0300

    ukkomogprog

--- colvb-master/examples/MOG_demo2.py
+++ colvb-master/examples/MOG_demo2.py
@@ -1,13 +1,19 @@
 # Copyright (c) 2012 James Hensman
 # Licensed under the GPL v3 (see LICENSE.txt)
 
+import time
+real_time = time.time()
 import numpy as np
 import pylab as pb
 import sys
-sys.path.append('..')
-sys.path.append('../colvb')
+sys.path.append('~/Windows/Desktop/hiit/HIITS15/colvb-master/colvb')
+sys.path.append('~/Windows/Desktop/hiit/HIITS15/colvb-master')
+sys.path.append('/cs/fs/home/othe/Windows/Desktop/hiit/HIITS15/colvb-master/colvb')
+#sys.path.append('..')
+#sys.path.append('../colvb')
 
 from MOG2 import MOG2
+
 pb.ion()
 
 np.random.seed(0)
@@ -18,20 +24,38 @@ dim = 2# orig = 2
 Nmin =3# orig = 25
 Nmax = 5# orig = 50
 Ndata = np.random.randint(Nmin, Nmax, Nclust)
-means = np.random.randn(Nclust, dim)*2 #orig = *5
+MeansCoeff = 2 #orig = 5
+means = np.random.randn(Nclust, dim)*MeansCoeff #orig = *5
 aa = [np.random.randn(dim, dim+1) for i in range(Nclust)]
 Sigmas = [np.dot(a, a.T) for a in aa]
 X = np.vstack([np.random.multivariate_normal(mu, cov, (n,)) for mu, cov, n in zip(means, Sigmas, Ndata)])/100
 Nrestarts= 3# orig = 3
 Nclust = 5# orig = 15
 
+
 m = MOG2(X, Nclust, prior_Z='DP')
-print 'N: ', m.N, ' K: ' , m.K
+
+#Print Stats
+print 'MOG_demo2.py'
+print 
+print'stats:'
+print 'N: ', m.N, ' K: ' , m.K, ' D: ', m.D
+print 'Nclust: ', Nclust, ' dim: ', dim
+print 'Nmin: ', Nmin, ' Nmax: ', Nmax
+print 'MeansCoeff: ', MeansCoeff
+print 'Nrestarts: ', Nrestarts
+print 'NClust: ', Nclust
+print
+
+#Make functions
+start_time = time.time()
 m.makeFunctions()
-print 'done'
+print 'Theano-function compilation time: ', '%s seconds' % (time.time() - start_time)
+print
 
 #starts = [np.random.randn(m.N*m.K) for i in range(Nrestarts)]
 
+#The steak
 from scipy.cluster import vq
 starts = []
 for i in range(Nrestarts):
@@ -39,17 +63,21 @@ for i in range(Nrestarts):
     dists = np.square(X[:,:,None]-means.T[None,:,:]).sum(1)
     starts.append(dists)
 
+main_time = time.time()
 for method in ['steepest', 'PR', 'FR', 'HS']:
     for st in starts:
+	print 'Start\nMethod used: ', method
+	start_time = time.time()
         m.set_vb_param(st)
         m.optimize(method=method, maxiter=1e4, opt=None, index='full', tests = None)
-        print method
+        print 'End\nMethod used: ', method, '\nRuntime: ', '%s seconds' % (time.time() - start_time) 
 
+print
+print 'Main time: ', '%s seconds' % (time.time() - main_time)
+print 'Total time: ', '%s seconds' % (time.time() - real_time)
 
-m.plot_tracks()
-m.plot()
-
-
+#m.plot_tracks()
+#m.plot()
 
 
 
