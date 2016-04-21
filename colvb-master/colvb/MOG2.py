overflow fix

colvb-master/colvb/MOG2.py
@@ -319,14 +319,18 @@ class MOG2(collapsed_mixture2):
 		Z *= pi[np.newaxis,:]
 		return Z.sum(1)
 
+	def test_mgrid(self):
+		pass
+
 	def plot(self, newfig=True):
+		import numpy as npp
 		if self.X.shape[1]==2:
 			if newfig:pb.figure()
 			xmin, ymin = self.X.min(0)
 			xmax, ymax = self.X.max(0)
 			xmin, xmax = xmin-0.1*(xmax-xmin), xmax+0.1*(xmax-xmin)
 			ymin, ymax = ymin-0.1*(ymax-ymin), ymax+0.1*(ymax-ymin)
-			xx, yy = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+			xx, yy = npp.mgrid[xmin:xmax:100j, ymin:ymax:100j]
 			Xgrid = np.vstack((xx.flatten(), yy.flatten())).T
 			zz = self.predict(Xgrid).reshape(100, 100)
 			zz_data = self.predict(self.X)

colvb-master/colvb/col_vb2.py
@@ -384,6 +384,9 @@ class col_vb2(GPy.core.model.Model, investigable):
 
 			grad,natgrad = self.vb_grad_natgrad()
 			grad,natgrad = -grad,-natgrad
+			print self.iteration
+			print self.bound_grad()(self.get_vb_param())
+			print self.vb_grad_natgrad()[0]
 			squareNorm = np.dot(natgrad,grad) # used to monitor convergence
 
 			self.road()
@@ -514,6 +517,9 @@ class col_vb2(GPy.core.model.Model, investigable):
 
 			grad, natgrad = self.bound_grad()(self.get_vb_param()), self.bound_grad(change=False)(self.get_param())
 			grad,natgrad = -grad,-natgrad
+			print self.iteration
+			print self.bound_grad()(self.get_vb_param())
+			print self.vb_grad_natgrad()[0]
 			squareNorm = np.dot(natgrad,grad) # used to monitor convergence
 
 			self.road()
@@ -604,7 +610,7 @@ class col_vb2(GPy.core.model.Model, investigable):
 		return self.autograd_bound()(self.get_vb_param())
 
 	def get_gradient(self):
-		return self.bound_gradient()(self.get_vb_param())
+		return self.bound_grad()(self.get_vb_param())
 
 	def get_hessian(self):
 		return self.bound_hessian()(self.get_vb_param())